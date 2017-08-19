43cfb1d8d1f578a173e4d215f923ecc0e913c2db
tibetd <tybetd@gmail.com>
2017-08-19 12:00:27 +0300

    TagInfo

---
+++ js/html.js
@@ -0,0 +1,189 @@
+﻿var htmlTags=[[{tag:"",desc:"",filename:""}]];
+var htmlTagCategory=[];
+htmlTagCategory[0]="Audio / Video";
+htmlTagCategory[1]="Basic HTML";
+htmlTagCategory[2]="Formatting";
+htmlTagCategory[3]="Forms and Input";
+htmlTagCategory[4]="Frames";
+htmlTagCategory[5]="Images";
+htmlTagCategory[6]="Links";
+htmlTagCategory[7]="Lists";
+htmlTagCategory[8]="Meta Info";
+htmlTagCategory[9]="Programming";
+htmlTagCategory[10]="Styles and Semantics";
+htmlTagCategory[11]="Tables";
+//htmlTagCategory[4]="";
+//Audio / Video
+htmlTags[0][0]={tag:"<audio>",desc:"Defines sound content",filename:"../tags/tag_audio.html"};
+htmlTags[0][1]={tag:"<source>",desc:"Defines multiple media resources for media elements (<video> and <audio>)",filename:"../tags/tag_source.html"};
+htmlTags[0][2]={tag:"<track>",desc:"Defines text tracks for media elements (<video> and <audio>)",filename:"../tags/tag_track.html"};
+htmlTags[0][3]={tag:"<video>",desc:"Defines a video or movie",filename:"../tags/tag_video.html"};
+htmlTags.push([]);//Basic HTML
+htmlTags[1][0]={tag:"<!--...-->",desc:"Defines a comment",filename:"../tags/tag_comment.html"};
+htmlTags[1][1]={tag:"<!DOCTYPE>",desc:"Defines the document type",filename:"../tags/tag_doctype.html"};
+htmlTags[1][2]={tag:"<body>",desc:"Defines the document's body",filename:"../tags/tag_body.html"};
+htmlTags[1][3]={tag:"<br>",desc:"Defines a single line break",filename:"../tags/tag_br.html"};
+htmlTags[1][4]={tag:"<h1> to <h6>",desc:"Defines HTML headings",filename:"../tags/tag_hn.html"};
+htmlTags[1][5]={tag:"<hr>",desc:"Defines a thematic change in the content",filename:"../tags/tag_hr.html"};
+htmlTags[1][6]={tag:"<html>",desc:"Defines the root of an HTML document",filename:"../tags/tag_html.html"};
+htmlTags[1][7]={tag:"<p>",desc:"Defines a paragraph",filename:"../tags/tag_p.html"};
+htmlTags[1][8]={tag:"<title>",desc:"Defines a title for the document",filename:"../tags/tag_title.html"};
+htmlTags.push([]);//Formatting
+htmlTags[2][0]={tag:"<abbr>",desc:"Defines an abbreviation or an acronym",filename:"../tags/tag_abbr.html"};
+htmlTags[2][1]={tag:"<acronym>",desc:"Not supported in HTML5. Use <abbr> instead.Defines an acronym",filename:"../tags/tag_acronym.html"};
+htmlTags[2][2]={tag:"<address>",desc:"Defines contact information for the author/owner of a document",filename:"../tags/tag_address.html"};
+htmlTags[2][3]={tag:"<b>",desc:"Defines bold text",filename:"tag_b.html"};
+htmlTags[2][4]={tag:"<bdi>",desc:"Isolates a part of text that might be formatted in a different direction from other text outside it",filename:"../tags/tags.html"};
+htmlTags[2][5]={tag:"<bdo>",desc:"Overrides the current text direction",filename:"../tags/tags.html"};
+htmlTags[2][6]={tag:"<big>",desc:"Not supported in HTML5. Use CSS instead.Defines big text",filename:"../tags/tags.html"};
+htmlTags[2][7]={tag:"<blockquote>",desc:"Defines a section that is quoted from another source",filename:"../tags/tags.html"};
+htmlTags[2][8]={tag:"<center>",desc:"Not supported in HTML5. Use CSS instead.Defines centered text",filename:"../tags/tags.html"};
+htmlTags[2][9]={tag:"<cite>",desc:"Defines the title of a work",filename:"../tags/tags.html"};
+htmlTags[2][10]={tag:"<code>",desc:"Defines a piece of computer code",filename:"../tags/tags.html"};
+htmlTags[2][11]={tag:"<del>",desc:"Defines text that has been deleted from a document",filename:"../tags/tags.html"};
+htmlTags[2][12]={tag:"<dfn>",desc:"Represents the defining instance of a term",filename:"../tags/tags.html"};
+htmlTags[2][13]={tag:"<em>",desc:"Defines emphasized text ",filename:"../tags/tags.html"};
+htmlTags[2][14]={tag:"<font>",desc:"Not supported in HTML5. Use CSS instead.Defines font, color, and size for text",filename:"../tags/tags.html"};
+htmlTags[2][15]={tag:"<i>",desc:"Defines a part of text in an alternate voice or mood",filename:"../tags/tags.html"};
+htmlTags[2][16]={tag:"<ins>",desc:"Defines a text that has been inserted into a document",filename:"../tags/tags.html"};
+htmlTags[2][17]={tag:"<kbd>",desc:"Defines keyboard input",filename:"../tags/tags.html"};
+htmlTags[2][18]={tag:"<mark>",desc:"Defines marked/highlighted text",filename:"../tags/tags.html"};
+htmlTags[2][19]={tag:"<meter>",desc:"Defines a scalar measurement within a known range (a gauge)",filename:"../tags/tags.html"};
+htmlTags[2][20]={tag:"<pre>",desc:"Defines preformatted text",filename:"../tags/tags.html"};
+htmlTags[2][21]={tag:"<progress>",desc:"Represents the progress of a task",filename:"../tags/tags.html"};
+htmlTags[2][22]={tag:"<q>",desc:"Defines a short quotation",filename:"../tags/tags.html"};
+htmlTags[2][23]={tag:"<rp>",desc:"Defines what to show in browsers that do not support ruby annotations",filename:"../tags/tags.html"};
+htmlTags[2][24]={tag:"<rt>",desc:"Defines an explanation/pronunciation of characters (for East Asian typography)",filename:"../tags/tags.html"};
+htmlTags[2][25]={tag:"<ruby>",desc:"Defines a ruby annotation (for East Asian typography)",filename:"../tags/tags.html"};
+htmlTags[2][26]={tag:"<s>",desc:"Defines text that is no longer correct",filename:"../tags/tags.html"};
+htmlTags[2][27]={tag:"<samp>",desc:"Defines sample output from a computer program",filename:"../tags/tags.html"};
+htmlTags[2][28]={tag:"<small>",desc:"Defines smaller text",filename:"../tags/tags.html"};
+htmlTags[2][29]={tag:"<strike>",desc:"Not supported in HTML5. Use <del> or <s> instead.Defines strikethrough text",filename:"../tags/tags.html"};
+htmlTags[2][30]={tag:"<strong>",desc:"Defines important text",filename:"../tags/tags.html"};
+htmlTags[2][31]={tag:"<sub>",desc:"Defines subscripted text",filename:"../tags/tags.html"};
+htmlTags[2][32]={tag:"<sup>",desc:"Defines superscripted text",filename:"../tags/tags.html"};
+htmlTags[2][33]={tag:"<time>",desc:"Defines a date/time",filename:"../tags/tags.html"};
+htmlTags[2][34]={tag:"<tt>",desc:"Not supported in HTML5. Use CSS instead.Defines teletype text",filename:"../tags/tags.html"};
+htmlTags[2][35]={tag:"<u>",desc:"Defines text that should be stylistically different from normal text",filename:"../tags/tags.html"};
+htmlTags[2][36]={tag:"<var>",desc:"Defines a variable",filename:"../tags/tags.html"};
+htmlTags[2][37]={tag:"<wbr>",desc:"Defines a possible line-break",filename:"../tags/tags.html"};
+htmlTags.push([]);//Forms and Input
+htmlTags[3][0]={tag:"<button>",desc:"Defines a clickable button",filename:"../tags/tags.html"};
+htmlTags[3][1]={tag:"<datalist>",desc:"Specifies a list of pre-defined options for input controls",filename:"../tags/tags.html"};
+htmlTags[3][2]={tag:"<fieldset>",desc:"Groups related elements in a form",filename:"../tags/tags.html"};
+htmlTags[3][3]={tag:"<form>",desc:"Defines an HTML form for user input",filename:"../tags/tags.html"};
+htmlTags[3][4]={tag:"<input>",desc:"Defines an input control",filename:"../tags/tags.html"};
+htmlTags[3][5]={tag:"<keygen>",desc:"Defines a key-pair generator field (for forms)",filename:"../tags/tags.html"};
+htmlTags[3][6]={tag:"<label>",desc:"Defines a label for an <input> element",filename:"../tags/tags.html"};
+htmlTags[3][7]={tag:"<legend>",desc:"Defines a caption for a <fieldset> element",filename:"../tags/tags.html"};
+htmlTags[3][8]={tag:"<optgroup>",desc:"Defines a group of related options in a drop-down list",filename:"../tags/tags.html"};
+htmlTags[3][9]={tag:"<option>",desc:"Defines an option in a drop-down list",filename:"../tags/tags.html"};
+htmlTags[3][10]={tag:"<output>",desc:"Defines the result of a calculation",filename:"../tags/tags.html"};
+htmlTags[3][11]={tag:"<select>",desc:"Defines a drop-down list",filename:"../tags/tags.html"};
+htmlTags[3][12]={tag:"<textarea>",desc:"Defines a multiline input control (text area)",filename:"../tags/tags.html"};
+htmlTags.push([]);//Frames
+htmlTags[4][0]={tag:"<frame>",desc:"Not supported in HTML5.Defines a window (a frame) in a frameset",filename:"../tags/tags.html"};
+htmlTags[4][1]={tag:"<frameset>",desc:"Not supported in HTML5.Defines a set of frames",filename:"../tags/tags.html"};
+htmlTags[4][2]={tag:"<iframe>",desc:"Defines an inline frame",filename:"../tags/tags.html"};
+htmlTags[4][3]={tag:"<noframes>",desc:"Not supported in HTML5.Defines an alternate content for users that do not support frames",filename:"../tags/tags.html"};
+htmlTags.push([]);//Images
+htmlTags[5][0]={tag:"<area>",desc:"Defines an area inside an image-map",filename:"../tags/tags.html"};
+htmlTags[5][1]={tag:"<canvas>",desc:"Used to draw graphics, on the fly, via scripting (usually JavaScript)",filename:"../tags/tags.html"};
+htmlTags[5][2]={tag:"<figcaption>",desc:"Defines a caption for a <figure> element",filename:"../tags/tags.html"};
+htmlTags[5][3]={tag:"<figure>",desc:"Specifies self-contained content",filename:"../tags/tags.html"};
+htmlTags[5][4]={tag:"<img>",desc:"Defines an image",filename:"../tags/tags.html"};
+htmlTags[5][5]={tag:"<map>",desc:"Defines a client-side image-map",filename:"../tags/tags.html"};
+htmlTags.push([]);//Links
+htmlTags[6][0]={tag:"<a>",desc:"Defines a hyperlink",filename:"../tags/tags.html"};
+htmlTags[6][1]={tag:"<link>",desc:"Defines the relationship between a document and an external resource (most used to link to style sheets)",filename:"../tags/tags.html"};
+htmlTags[6][2]={tag:"<nav>",desc:"Defines navigation links",filename:"../tags/tags.html"};
+htmlTags.push([]);//Lists
+htmlTags[7][0]={tag:"<dd>",desc:"Defines a description/value of a term in a description list",filename:"../tags/tags.html"};
+htmlTags[7][1]={tag:"<dir>",desc:"Not supported in HTML5. Use <ul> instead.Defines a directory list",filename:"../tags/tags.html"};
+htmlTags[7][2]={tag:"<dl>",desc:"Defines a description list",filename:"../tags/tags.html"};
+htmlTags[7][3]={tag:"<dt>",desc:"Defines a term/name in a description list",filename:"../tags/tags.html"};
+htmlTags[7][4]={tag:"<li>",desc:"Defines a list item",filename:"../tags/tags.html"};
+htmlTags[7][5]={tag:"<menu>",desc:"Defines a list/menu of commands",filename:"../tags/tags.html"};
+htmlTags[7][6]={tag:"<menuitem>",desc:"Defines a command/menu item that the user can invoke from a popup menu",filename:"../tags/tags.html"};
+htmlTags[7][7]={tag:"<ol>",desc:"Defines an ordered list",filename:"../tags/tags.html"};
+htmlTags[7][8]={tag:"<ul>",desc:"Defines an unordered list",filename:"../tags/tags.html"};
+htmlTags.push([]);//Meta Info
+htmlTags[8][0]={tag:"<base>",desc:"Specifies the base URL/target for all relative URLs in a document",filename:"../tags/tags.html"};
+htmlTags[8][1]={tag:"<basefont>",desc:"Not supported in HTML5. Use CSS instead.Specifies a default color, size, and font for all text in a document",filename:"../tags/tags.html"};
+htmlTags[8][2]={tag:"<head>",desc:"Defines information about the document",filename:"../tags/tags.html"};
+htmlTags[8][3]={tag:"<meta>",desc:"Defines metadata about an HTML document",filename:"../tags/tags.html"};
+htmlTags.push([]);//Programming
+htmlTags[9][0]={tag:"<applet>",desc:"Not supported in HTML5. Use <embed> or <object> instead.Defines an embedded applet",filename:"../tags/tags.html"};
+htmlTags[9][1]={tag:"<embed>",desc:"Defines a container for an external (non-HTML) application",filename:"../tags/tags.html"};
+htmlTags[9][2]={tag:"<noscript>",desc:"Defines an alternate content for users that do not support client-side scripts",filename:"../tags/tags.html"};
+htmlTags[9][3]={tag:"<object>",desc:"Defines an embedded object",filename:"../tags/tags.html"};
+htmlTags[9][4]={tag:"<param>",desc:"Defines a parameter for an object",filename:"../tags/tags.html"};
+htmlTags[9][5]={tag:"<script>",desc:"Defines a client-side script",filename:"../tags/tags.html"};
+htmlTags.push([]);//Styles and Semantics
+htmlTags[10][0]={tag:"<article>",desc:"Defines an article",filename:"../tags/tags.html"};
+htmlTags[10][1]={tag:"<aside>",desc:"Defines content aside from the page content",filename:"../tags/tags.html"};
+htmlTags[10][2]={tag:"<details>",desc:"Defines additional details that the user can view or hide",filename:"../tags/tags.html"};
+htmlTags[10][3]={tag:"<dialog>",desc:"Defines a dialog box or window",filename:"../tags/tags.html"};
+htmlTags[10][4]={tag:"<div>",desc:"Defines a section in a document",filename:"../tags/tags.html"};
+htmlTags[10][5]={tag:"<footer>",desc:"Defines a footer for a document or section",filename:"../tags/tags.html"};
+htmlTags[10][6]={tag:"<header>",desc:"Defines a header for a document or section",filename:"../tags/tags.html"};
+htmlTags[10][7]={tag:"<main>",desc:"Specifies the main content of a document",filename:"../tags/tags.html"};
+htmlTags[10][8]={tag:"<section>",desc:"Defines a section in a document",filename:"../tags/tags.html"};
+htmlTags[10][9]={tag:"<span>",desc:"Defines a section in a document",filename:"../tags/tags.html"};
+htmlTags[10][10]={tag:"<style>",desc:"Defines style information for a document",filename:"../tags/tags.html"};
+htmlTags[10][11]={tag:"<summary>",desc:"Defines a visible heading for a <details> element",filename:"../tags/tags.html"};
+htmlTags.push([]);//Tables
+htmlTags[11][0]={tag:"<caption>",desc:"Defines a table caption",filename:"../tags/tags.html"};
+htmlTags[11][1]={tag:"<col>",desc:"Specifies column properties for each column within a <colgroup> element ",filename:"../tags/tags.html"};
+htmlTags[11][2]={tag:"<colgroup>",desc:"Specifies a group of one or more columns in a table for formatting",filename:"../tags/tags.html"};
+htmlTags[11][3]={tag:"<table>",desc:"Defines a table",filename:"../tags/tags.html"};
+htmlTags[11][4]={tag:"<tbody>",desc:"Groups the body content in a table",filename:"../tags/tags.html"};
+htmlTags[11][5]={tag:"<td>",desc:"Defines a cell in a table",filename:"../tags/tags.html"};
+htmlTags[11][6]={tag:"<tfoot>",desc:"Groups the footer content in a table",filename:"../tags/tags.html"};
+htmlTags[11][7]={tag:"<th>",desc:"Defines a header cell in a table",filename:"../tags/tags.html"};
+htmlTags[11][8]={tag:"<thead>",desc:"Groups the header content in a table",filename:"../tags/tags.html"};
+htmlTags[11][9]={tag:"<tr>",desc:"Defines a row in a table",filename:"../tags/tags.html"};
+
+
+
+
+
+
+function fillSelectWithhmtlTags(selectid)
+{
+    var slct = document.getElementById(selectid);  
+	for (var ic = 0; ic < htmlTagCategory.length; ic++)
+	{
+		slct.innerHTML+="<optgroup label=\"" + htmlTagCategory[ic] +"\"></optgroup>";		
+		//var s="";
+		for (var it=0;it<htmlTags[ic].length;it++)
+		{
+			// Create a <button> element
+			//var t = document.createTextNode("CLICK ME");       // Create a text node
+			//slct.appendChild(t);                                // Append the text to <button>
+			var opt = document.createElement("OPTION"); 
+			opt.label=htmlTags[ic][it].tag;
+			opt.value=[ic]+","+[it];
+			slct.appendChild(opt);
+			//document.body.appendChild(slct);                    // Append <button> to <body>
+			//s+="<option>&lt;"+htmlTags[index][k].tag+"&gt;</option>";
+		}
+		
+	}
+}
+
+function getic(selectid)
+{
+return selectid.value.substring(0,tagSelect.value.indexOf(",")); // category index
+
+}
+
+
+function getit(selectid)
+{
+return selectid.value.substring(tagSelect.value.indexOf(",")+1,tagSelect.value.length); //tag index
+}
+
+
+
+
---
+++ js/nnb.js
@@ -0,0 +1,10 @@
+﻿function aa()
+{
+	for (var k=0;k<document.getElementsByTagName("DIV").length;k++)
+	{
+		if (document.getElementsByTagName("DIV")[k].className=="w3-example")
+		{
+			document.getElementsByTagName("DIV")[k].style.display="none";
+		}
+	}
+}
